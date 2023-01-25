c28fafebf26aedc71e9c12e860be6a3a770a5f68
rassellek <rassellek@bk.ru>
2023-01-25 13:13:21 +0000

    Deploying to gh-pages from @ rassellek/test_quest@325ae9aad5723b1b47be6aca0a334d5672b7d1f8 🚀

---
+++ _next/static/iiVV3F-BDZQ9umqVWzE5W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/693-ee499cb4e62b2efc.js","static/css/320a4c87177ea2d7.css","static/chunks/pages/index-26f6a71fc239c78e.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
